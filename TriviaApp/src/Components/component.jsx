3e3eec591b7adfa6aaef35504ded7bf25b05bb5f
alankze <alan.zin.eddin@gmail.com>
2023-07-05 13:32:25 +0200

    firstComponent

---
+++ TriviaApp/src/Components/component.jsx
@@ -0,0 +1,12 @@
+<><template>
+    <div>
+        <h1>{{ title }}</h1>
+        <p>{{ content }}</p>
+    </div>
+</template><script>
+        export default {}: 'component',
+        props: {title}: String,
+        content: String
+        }
+        }
+    </script></>
